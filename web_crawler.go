0485fab4bc7a96535b9c14ac3b187fe94c08204e
bramdekker <b.dekker16@hotmail.com>
2020-02-01 18:38:46 +0100

    Exercise Web Crawler done!

--- web_crawler.go
+++ web_crawler.go
@@ -1,7 +1,34 @@
 package main
+
 import (
 	"fmt"
+	"sync"
 )
+
+var wg sync.WaitGroup
+
+// SafeMap is safe to use concurrently.
+type SafeMap struct {
+	v   map[string]bool
+	mux sync.Mutex
+}
+
+// Set sets the value for the given key.
+func (c *SafeMap) Set(key string) {
+	c.mux.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	c.v[key] = true
+	c.mux.Unlock()
+}
+
+// Value returns the current value of the counter for the given key.
+func (c *SafeMap) Value(key string) bool {
+	c.mux.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	defer c.mux.Unlock()
+	_, ok := c.v[key]
+	return ok
+}
 
 type Fetcher interface {
 	// Fetch returns the body of URL and
@@ -11,26 +38,41 @@ type Fetcher interface {
 
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
-func Crawl(url string, depth int, fetcher Fetcher) {
+func Crawl(url string, depth int, fetcher Fetcher, s SafeMap) {
 	// TODO: Fetch URLs in parallel.
 	// TODO: Don't fetch the same URL twice.
 	// This implementation doesn't do either:
+	defer wg.Done()
+
 	if depth <= 0 {
 		return
 	}
+
+	if s.Value(url) {
+		return
+	}
+
+	s.Set(url)
+
 	body, urls, err := fetcher.Fetch(url)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Printf("found: %s %q\n", url, body)
+
 	for _, u := range urls {
-		Crawl(u, depth-1, fetcher)
+		wg.Add(1)
+		go Crawl(u, depth-1, fetcher, s)
 	}
 	return
+}
 
-}func main() {
-    Crawl("https://golang.org/", 4, fetcher)
+func main() {
+	mapURL := SafeMap{v: make(map[string]bool)}
+	wg.Add(1)
+	go Crawl("https://golang.org/", 4, fetcher, mapURL)
+	wg.Wait()
 }
 
 // fakeFetcher is Fetcher that returns canned results.
